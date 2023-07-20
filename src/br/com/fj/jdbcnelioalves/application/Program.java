94e25d2cee1384a1bdb76fad18afc7fc5924321e
Flavio Milani <frmilanij@gmail.com>
2023-07-20 12:05:51 -0300

    Seller entity class

--- src/br/com/fj/jdbcnelioalves/application/Program.java
+++ src/br/com/fj/jdbcnelioalves/application/Program.java
@@ -1,14 +1,19 @@
 package br.com.fj.jdbcnelioalves.application;
 
+
+import java.util.Date;
+
 import br.com.fj.jdbcnelioalves.model.entities.Department;
+import br.com.fj.jdbcnelioalves.model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
+		Department department = new Department(1, "books");
+		Seller seller = new Seller(21, "Bob", "bob@dominio.com", new Date(), 3000.0, department);
 		
-		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
