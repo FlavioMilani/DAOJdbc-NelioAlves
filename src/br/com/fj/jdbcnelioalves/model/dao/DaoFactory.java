51b991698bc7c4cd3039153cab2c14868c78334d
Flavio Milani <frmilanij@gmail.com>
2023-07-21 04:54:26 -0300

    DepartmentDaoJDBC implementation

--- src/br/com/fj/jdbcnelioalves/model/dao/DaoFactory.java
+++ src/br/com/fj/jdbcnelioalves/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package br.com.fj.jdbcnelioalves.model.dao;
 
 import br.com.fj.jdbcnelioalves.db.DataBase;
+import br.com.fj.jdbcnelioalves.model.dao.impl.DepartmentDaoJDBC;
 import br.com.fj.jdbcnelioalves.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDAO createSellerDao() {
 		return new SellerDaoJDBC(DataBase.getConnection());
+	}
+	
+	public static DepartmentDAO createDepartmentDao() {
+		return new DepartmentDaoJDBC(DataBase.getConnection());
 	}
 }
